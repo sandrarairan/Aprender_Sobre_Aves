76d4be3a69b075113b9a63b6a71a323a0a5def62
Sandra Rairan <sandrarairan@gmail.com>
2020-07-07 15:55:02 -0500

    Actualizacion_imagenes_aves

--- app.R
+++ app.R
@@ -3,19 +3,22 @@ library(rlist)
 library(shinyWidgets)
 
 # Para que se mustren las imagenes y videos es necesario crear una carpeta de www
-fileaudio<-list.files("www/",pattern="(.wav)") # produce un vector de caracteres de los nombres de archivos [1] "Sicalis flaveola.wav"  "Spinus psaltria.wav"   "Synallaxis azarae.wav"
-file_audio<-sapply(fileaudio,function(x) tools::file_path_sans_ext(x),USE.NAMES = F) # devuelve las rutas de archivo sin extensiones (y el punto inicial) [1] "Sicalis flaveola"  "Spinus psaltria"   "Synallaxis azarae"
+#fileaudio<-list.files("www/",pattern="(.wav)") # produce un vector de caracteres de los nombres de archivos [1] "Sicalis flaveola.wav"  "Spinus psaltria.wav"   "Synallaxis azarae.wav"
+#file_audio<-sapply(fileaudio,function(x) tools::file_path_sans_ext(x),USE.NAMES = F) # devuelve las rutas de archivo sin extensiones (y el punto inicial) [1] "Sicalis flaveola"  "Spinus psaltria"   "Synallaxis azarae"
+nombrecientifico<-list.files("www/",pattern="(.jpg)")
+nombre_cientifico<-sapply(nombrecientifico,function(x) tools::file_path_sans_ext(x),USE.NAMES = F)
 
 
-filevideo<-list.files("www/",pattern="(.mp4)") 
-file_video<-sapply(filevideo,function(x) tools::file_path_sans_ext(x),USE.NAMES = F) #
+#filevideo<-list.files("www/",pattern="(.mp4)") 
+#file_video<-sapply(filevideo,function(x) tools::file_path_sans_ext(x),USE.NAMES = F) #
 
-imgname<-sapply(file_audio,function(x) gsub(pattern="_.+$",replacement="",x))
-imgfile<-paste0(imgname,".jpg")
-specfile<-paste0(file_audio,"_especto.png")
-#videofile<-paste0(imgname,".mp4")
-answers<-sapply(file_audio,function(x) gsub("_XC.*","",x),USE.NAMES = F)
+imgname<-sapply(nombre_cientifico,function(x) gsub(pattern="_.+$",replacement="",x))
+imgfile<-paste0(imgname,".wav")
+specfile<-paste0(nombre_cientifico,"_especto.png")
+videofile<-paste0(imgname,".mp4")
+answers<-sapply(nombre_cientifico,function(x) gsub("_XC.*","",x),USE.NAMES = F)
 
+#nombrecientifico<-list.files("www/",pattern="(.jpg)")
 familia<-c("Thraupidae","Fringillidae","Furnariidae")
 
 
@@ -102,13 +105,13 @@ ui <- navbarPage(title="Aves Reserva de Castilla",position="fixed-top",theme=shi
                            
                           mainPanel(class="body",width=10,
                                   
-                                    lapply(1:length(file_audio),function(i) {
+                                    lapply(1:length(nombre_cientifico),function(i) {
                                       wellPanel(fluidRow(
                                         h3(paste0(i,": ",answers[i])),
-                                        img(src=imgfile[i],height="200px",width="auto"),
+                                        img(src=nombrecientifico[i],height="200px",width="auto"),
                                         img(src=specfile[i]),
-                                        tags$audio(src=fileaudio[i],type = "audio/wav", controls = "false"),
-                                        tags$video(src=filevideo[i],type = "video/mp4", autoplay =NA , controls =NA,height="200px",width="auto"),
+                                        tags$audio(src=imgfile[i],type = "audio/wav", controls = "false"),
+                                        tags$video(src=videofile[i],type = "video/mp4", autoplay ="false" , controls =NA,height="200px",width="auto"),
                                         #tags$video(id="ave", type = "video/mp4",src = "Sicalisflaveola.mp4", controls = "controls")
                                         
                                       )
@@ -127,10 +130,10 @@ ui <- navbarPage(title="Aves Reserva de Castilla",position="fixed-top",theme=shi
                                        h4("Verbatim text output"),
                                        verbatimTextOutput("txtout"),
                                        h5("Header 5"),
-                                       lapply(1:length(file_audio),function(i) {
+                                       lapply(1:length(nombre_cientifico),function(i) {
                                          wellPanel(fluidRow(
                                            h3(paste0(i,": ",answers[i])),
-                                           img(src=imgfile[i],height="200px",width="auto"),
+                                           img(src=nombrecientifico[i],height="200px",width="auto"),
                                            h4(familia[i]),
                                            tags$h5(familia[i]),
                                            
@@ -158,18 +161,18 @@ server <- function(input, output) {
   vals<-reactiveValues()
   vals$counter=1
   
-  fileaudio<-list.files("www/",pattern="(.wav)") #find audio files; 
-  file_audio<-sapply(file_audio,function(x) tools::file_path_sans_ext(x),USE.NAMES = F) 
+  nombrecientifico<-list.files("www/",pattern="(..jpg)") #find audio files; 
+  nombre_cientifico<-sapply(nombre_cientifico,function(x) tools::file_path_sans_ext(x),USE.NAMES = F) 
   
   #Sample the files initially (random)
   isolate({
-    vals$ordr<-sample(1:length(file_audio),length(file_audio),replace=F)
+    vals$ordr<-sample(1:length(nombre_cientifico),length(nombre_cientifico),replace=F)
   })
   
   #Reset Button
   observeEvent(input$reset,{
     vals$counter <- 1
-    vals$ordr<-sample(1:length(file_audio),length(file_audio),replace=F)
+    vals$ordr<-sample(1:length(nombre_cientifico),length(nombre_cientifico),replace=F)
     print(paste0("counter=",vals$counter))
     print(paste0("order=",vals$ordr))
     
@@ -179,7 +182,7 @@ server <- function(input, output) {
   observeEvent(input$call,{
     vals$counter=vals$counter+1
     print("---------------------")
-    key<-as.matrix(t(rbind(1:vals$counter,file_audio[vals$ordr[vals$counter]])))
+    key<-as.matrix(t(rbind(1:vals$counter,nombre_cientifico[vals$ordr[vals$counter]])))
     rownames(key)<-rep("",vals$counter)
     colnames(key)<-c("Sound","Answer")
     print(key)
@@ -193,13 +196,13 @@ server <- function(input, output) {
       #which sample of the randomized order are we on?
       whichSample<-vals$ordr[i]
       #assemble pieces of well panel
-      audio<-if(input$showaudio){tags$audio(src=fileaudio[whichSample],type = 'audio/wav', controls = 'false')}else{}
+      audio<-if(input$showaudio){tags$audio(src=imgfile[whichSample],type = 'audio/wav', controls = 'false')}else{}
       
-      pic<- if(input$showpic){img(src=imgfile[whichSample],height="200px",width="auto")}else{}
+      pic<- if(input$showpic){img(src=nombrecientifico[whichSample],height="200px",width="auto")}else{}
       
       spec<- if(input$showspec){img(src=specfile[whichSample])}else{}
       
-      video<-if(input$showvideo){tags$video(src=filevideo[whichSample],type = 'video/mp4', controls = NA,height="200px",width="auto")}else{}
+      video<-if(input$showvideo){tags$video(src=videofile[whichSample],type = 'video/mp4', controls = NA,height="200px",width="auto")}else{}
       
       #Muestra la ave ó oculta la información del ave
       text<- if(input$reveal){h3(paste0(i,": ",answers[whichSample]))}else{paste0("¿ Qué Ave es ? ",i)}
